6e860ae6d7b3bb0b798f04266a0ad78f0ad46baa
luutien18195 <30953080+luutien18195@users.noreply.github.com>
2023-08-01 23:37:07 +0700

    Update valid_token api

--- src/main/java/com/tainika/qlnt/qlnt/controller/BaseController.java
+++ src/main/java/com/tainika/qlnt/qlnt/controller/BaseController.java
@@ -2,15 +2,19 @@ package com.tainika.qlnt.qlnt.controller;
 
 import com.tainika.qlnt.qlnt.dto.auth.AuthRequest;
 import com.tainika.qlnt.qlnt.dto.auth.AuthResponse;
+import com.tainika.qlnt.qlnt.model.UserLoginDetails;
 import com.tainika.qlnt.qlnt.service.UserLoginDetailsService;
 import com.tainika.qlnt.qlnt.ultil.JwtUtil;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -35,5 +39,16 @@ public class BaseController {
         final String jwt = jwtUtil.generateToken(userDetails);
 
         return ResponseEntity.ok(new AuthResponse(jwt));
+    }
+
+    @PostMapping("/valid_token")
+    public ResponseEntity<String> validToken(@RequestBody AuthRequest auth) {
+        UserLoginDetails loginUser = (UserLoginDetails) SecurityContextHolder
+            .getContext()
+            .getAuthentication()
+            .getPrincipal();
+        if (!loginUser.getUsername().equals(auth.getUsername())) return new ResponseEntity<>(HttpStatus.FORBIDDEN);
+
+        return ResponseEntity.ok(loginUser.getUsername());
     }
 }
